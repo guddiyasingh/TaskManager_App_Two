1bd604fe10bb839f17643c9e51ebf4e55dba94af
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-03-31 23:23:53 +0530

    Backend file

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import {BrowserRouter as Router,
   Routes,
   Route,
-}from "react-router-dom"
-import PrivateRoute from './routes/PrivateRoute'
-import Dashboard from './pages/User/Dashboard'
+}from "react-router-dom";
+
+import Dashboard from "./pages/Admin/Dashboard";
+import Login from "./pages/Auth/Login";
+import SignnUp from "./pages/Auth/SignUp";
+import ManageTasks from "./pages/Admin/ManageTasks";
+import CreateTask from "./pages/Admin/CreatTask";
+import ManageUsers from "./pages/Admin/ManageUsers";
+import UserDashboard from "./pages/User/UserDashboard";
+import MyTasks from "./pages/User/MyTasks";
+import ViewTaskDetails from './pages/User/ViewTaskDetails';
+import PrivateRoute from "./routes/PrivateRoute"
 
 const App = () => {
   return (
@@ -13,14 +22,24 @@ const App = () => {
   <Routes>
     <Route path="/login" element={<Login />}/>
     <Route path='/signUp' element ={<SignnUp/>}/>
+
+
     {/*Admin Routes */}
 
-
-    <Route element = {<PrivateRoute allowedRoles={["admin"]}/>}>
+   <Route element = {<PrivateRoute allowedRoles={["admin"]}/>}>
     <Route path='/admin/dashboard' element = {<Dashboard />} />
     <Route path="admin/tasks" element={<ManageTasks/>}/>
     <Route path='/admin/crreate-tasks' element={<CreateTask/>} />
     <Route path='/admin/users' element={<ManageUsers/>} />
+    </Route>
+     
+   {/* User Routes */}
+
+    <Route element = {<PrivateRoute allowedRoles={["admin"]}/>}>
+    <Route path='/user/dashboard' element = {<UserDashboard />} />
+    <Route path='user/tasks' element = {<MyTasks/>}/>
+    <Route path='user/task-details/:id' element= {<ViewTaskDetails/>}/>
+   
     </Route>
     </Routes>
   </Router>
--- Frontend/my-app/src/routes/PrivateRoute.jsx
+++ Frontend/my-app/src/routes/PrivateRoute.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
-const PrivateRoute = () => {
-  return (
-    <div>
-      PrivateRoute
-    </div>
+const PrivateRoute = ({allowedRoles}) => {
+  return ( <Outlet/>
+   
   )
 }
 
