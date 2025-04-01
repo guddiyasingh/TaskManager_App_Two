8f02e2aab8f628aeee6677eeb9da070f92c612da
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-01 16:13:39 +0530

    add db.js file code

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require(path);
+const path = require("path");
 
 const app = express();
 
