8b6519cb7ab72b7f0cc6f6cf75674c587e5b9742
Guddiya Singh <guddiyasingh1847@gmail.com>
2025-04-02 00:00:33 +0530

    User code

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -16,8 +17,12 @@ app.use (
     })
 );
 
-//Middleware
 
+
+// Connect Database
+   connectDB();
+
+//Middleware
 app.use(express.json());
 
 //Routes
